3e4e3c7b7c2b8f88e9e91f3b349c330621bee29f
orkait <66670953+KailasMahavarkar@users.noreply.github.com>
2025-03-28 07:01:12 +0530

    sync update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,33 @@
 const nextConfig = {
 	experimental: {
 		appDir: true,
-	}
+	},
+	images: {
+		domains: ["i.imgur.com", "raw.githubusercontent.com", "*"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
+				port: "",
+				pathname: "/**",
+				search: "",
+			},
+			{
+				protocol: "https",
+				hostname: "raw.githubusercontent.com",
+				port: "",
+				pathname: "/**",
+				search: "",
+			},
+            {
+				protocol: "https",
+				hostname: "*",
+				port: "",
+				pathname: "/**",
+				search: "",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
